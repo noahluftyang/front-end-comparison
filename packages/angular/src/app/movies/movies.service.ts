f42a91e3e7c02d69bd081361115607e1a8025153
noahluftyang <euihyun.yang.9x@gmail.com>
2019-11-03 16:58:40 +0900

    Add movies api service

--- packages/angular/src/app/movies/movies.service.ts
+++ packages/angular/src/app/movies/movies.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  private http: HttpClient
+
   movies = [];
+
+  getMovies() {
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?${stringify({
+      api_key: process.env.ANGULAR_TMDB_API_KEY
+    })}`)
+  }
 }
